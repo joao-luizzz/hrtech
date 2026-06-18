7c29cb06b2a0faeb93de4d6e49275f2dedbd690e
joao-luizzz <luizjoaoprofissional@gmail.com>
2026-06-18 00:52:49 -0300

    fix(deploy): decouple db migrations from build script for render

--- build.sh
+++ build.sh
@@ -10,9 +10,3 @@ pip install -r requirements.txt
 
 # 3. Reúne os ficheiros estáticos (CSS, JS) para o Render
 python manage.py collectstatic --no-input
-
-# 4. Atualiza as tabelas da Base de Dados na nuvem
-python manage.py migrate
-
-# 5. Roda o nosso comando para criar o admin do RH (Só roda na nuvem)
-python manage.py setup_rh
